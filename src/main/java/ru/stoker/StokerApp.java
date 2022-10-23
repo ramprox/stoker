287030b7e967ad71b47d0834c70595bf6a655183
ramil <ram-kur@mail.ru>
2022-10-23 09:45:46 +0400

    Start default web server

---
+++ src/main/java/ru/stoker/StokerApp.java
@@ -0,0 +1,13 @@
+package ru.stoker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StokerApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StokerApp.class, args);
+    }
+
+}
